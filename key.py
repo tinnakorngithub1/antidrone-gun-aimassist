1ad6e10a6343130aa1368788c5bed188b7191743
tinnakorngithub1 <85947783+tinnakorngithub1@users.noreply.github.com>
2026-07-02 22:04:54 +0700

    Add files via upload

---
+++ key.py
@@ -0,0 +1,4 @@
+mkdir -p ~/.ssh && chmod 700 ~/.ssh
+  echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4AdONH5jMk0MZEstbKEUkBOFEjiEH8ynDNxMZZmjwb claude-survey-254-to-arm' >>
+  ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/authorized_keys
